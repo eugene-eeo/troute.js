176396fd542e0ec09a5d72d4ba85d250c7faec7f
Eeo Jun <packwolf58@gmail.com>
2015-12-26 22:43:19 +0800

    Compress even further

--- troute.js
+++ troute.js
@@ -1,50 +1,44 @@
 troute = function() {
   function split(url) {
-    if (url[0] == '/')            url = url.slice(1);
-    if (url[url.length-1] == '/') url = url.slice(0, -1);
-    return url.split('/');
+    return url.replace(/^\/|\/($|\?)/g, '')
+              .split('/');
   };
 
   var enc = encodeURIComponent;
   var dnc = decodeURIComponent;
-
   var tree = {};
 
-  var add = function(url, cb) {
-    var u = split(url);
-    var t = tree;
-    for (var i = 0; i < u.length; i++) {
-      var part = u[i];
-      var capt = part[0] == ':';
-      var name = capt ? ':' : enc(part);
-      t = t[name] || (t[name] = {});
-      if (capt)
-        t['~'] = part.slice(1);
-    }
-    t['/'] = cb;
-  };
-
-  var lookup = function(url) {
-    var u = split(url);
-    var t = tree;
-    var params = {};
-
-    for (var i = 0; i < u.length; i++) {
-      var p = u[i];
-      var q = dnc(p);
-
-      t = t[enc(q.toLowerCase())] || t[':'];
-      if (!t) return;
-      t['~'] && (params[t['~']] = q);
-    }
-    return ('/' in t) && {
-      params: params,
-      cb: t['/'],
-    };
-  };
-
   return {
-    add: add,
-    lookup: lookup
+    add: function(url, cb) {
+      var u = split(url);
+      var t = tree;
+      for (var i = 0; i < u.length; i++) {
+        var part = u[i];
+        var capt = part[0] == ':';
+        var name = capt ? ':' : enc(part);
+        t = t[name] || (t[name] = {});
+        if (capt)
+          t['~'] = part.slice(1);
+      }
+      t['/'] = cb;
+    },
+    
+    lookup: function(url) {
+      var u = split(url);
+      var t = tree;
+      var params = {};
+
+      for (var i = 0; i < u.length; i++) {
+        var q = dnc(u[i]);
+        t = t[enc(q.toLowerCase())] || t[':'];
+        if (!t) return;
+        t['~'] && (params[t['~']] = q);
+      }
+      if ('/' in t)
+        return {
+          params: params,
+          cb: t['/'],
+        };
+    }
   };
 };
