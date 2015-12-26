99dc636fc1305884c9f42cfeff60c8f2b0565473
Eeo Jun <packwolf58@gmail.com>
2015-12-26 22:44:32 +0800

    Slight refactor

--- troute.js
+++ troute.js
@@ -8,37 +8,40 @@ troute = function() {
   var dnc = decodeURIComponent;
   var tree = {};
 
-  return {
-    add: function(url, cb) {
-      var u = split(url);
-      var t = tree;
-      for (var i = 0; i < u.length; i++) {
-        var part = u[i];
-        var capt = part[0] == ':';
-        var name = capt ? ':' : enc(part);
-        t = t[name] || (t[name] = {});
-        if (capt)
-          t['~'] = part.slice(1);
-      }
-      t['/'] = cb;
-    },
-    
-    lookup: function(url) {
-      var u = split(url);
-      var t = tree;
-      var params = {};
-
-      for (var i = 0; i < u.length; i++) {
-        var q = dnc(u[i]);
-        t = t[enc(q.toLowerCase())] || t[':'];
-        if (!t) return;
-        t['~'] && (params[t['~']] = q);
-      }
-      if ('/' in t)
-        return {
-          params: params,
-          cb: t['/'],
-        };
+  function add(url, cb) {
+    var u = split(url);
+    var t = tree;
+    for (var i = 0; i < u.length; i++) {
+      var part = u[i];
+      var capt = part[0] == ':';
+      var name = capt ? ':' : enc(part);
+      t = t[name] || (t[name] = {});
+      if (capt)
+        t['~'] = part.slice(1);
     }
+    t['/'] = cb;
+  };
+
+  function lookup(url) {
+    var u = split(url);
+    var t = tree;
+    var params = {};
+
+    for (var i = 0; i < u.length; i++) {
+      var q = dnc(u[i]);
+      t = t[enc(q.toLowerCase())] || t[':'];
+      if (!t) return;
+      t['~'] && (params[t['~']] = q);
+    }
+    if ('/' in t)
+      return {
+        params: params,
+        cb: t['/'],
+      };
+  };
+
+  return {
+    add: add,
+    lookup: lookup
   };
 };
