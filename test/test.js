295c584f3a3164368a46e7067bdee7b13f9c2abc
Eeo Jun <packwolf58@gmail.com>
2015-12-26 12:45:44 +0800

    Update tests

--- test/test.js
+++ test/test.js
@@ -1,19 +1,16 @@
 suite('troute.lookup', function() {
+  var r = troute();
+
+  r.add('user/',      0);
+  r.add('user/:name', 1);
+  r.add('user/john',  2);
+
   test('returns the most specific match', function() {
-    var r = troute();
-
-    r.add('user/:name', 1);
-    r.add('user/john',  2);
-
     var match = r.lookup('user/john');
     assert(match.data === 2);
   });
 
   test('sanitises the url', function() {
-    var r = troute();
-
-    r.add('user', 1);
-
     var urls = [
       '/user/',
       '/user',
@@ -21,12 +18,21 @@ suite('troute.lookup', function() {
     ];
     for (var i=urls.length; i--;) {
       var match = r.lookup(urls[i]);
-      assert(match.data === 1);
+      assert(match.data === 0);
     }
   });
 
+  test('ignores the case when matching', function() {
+    var match = r.lookup('/User/JoHN');
+    assert(match.data === 2);
+  });
+
+  test('preserves case in parameters', function() {
+    var match = r.lookup('/user/ONE');
+    assert(match.params.name === 'ONE');
+  });
+
   test('returns null or undefined if theres no match', function() {
-    var r = troute();
     assert(!r.lookup('/'));
   });
 });
